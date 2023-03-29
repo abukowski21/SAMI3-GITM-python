189caed457ace985922c9639e480ad35d398e474
Aaron Bukowski <abukowski21@gmail.com>
2023-03-29 11:57:24 -0500

    create file. draft outline.

---
+++ AllPlots.py
@@ -0,0 +1,17 @@
+"""Here we will try to make all of the available plots... 
+Let's see if it's possible!
+"""
+
+
+"""
+- first get the arguments.
+- then read in data specified
+    - this will be either gitm, sami, or both
+- then get the data we want 
+    - e.g. diff from filter, diff btwn runs, etc.
+- then launch plotting routines
+    - keos, maps, polar dials, etc.
+    - save plots as we go, print out dirs for ffmpeg
+        - and/or a ffmpeg command.
+- exit gracefully?
+"""
