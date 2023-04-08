137631029f9839086a64f80fd5e578b36e37f343
Aaron Bukowski <abukowski21@gmail.com>
2023-04-08 15:18:09 -0600

    Capability to make subplots with all fig types.

--- utility_programs/plotting_routines.py
+++ utility_programs/plotting_routines.py
@@ -2,7 +2,7 @@ import os
 from matplotlib import pyplot as plt
 import geopandas
 import time
-from scipy.interpolate import LinearNDInterpolator
+from scipy.interpolate import LinearNDInterpolator, interp1d
 import numpy as np
 
 world = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
@@ -10,13 +10,15 @@ world = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
 
 def make_a_keo(
         arr,
-        title,
-        cbarlims,
-        cbar_name,
+        title = None,
+        cbarlims = None,
+        cbar_name = None,
         y_label="Latitude (deg)",
         x_label="Hours since storm onset",
         save_or_show="save",
+        cmap='viridis',
         fname=None,
+        ax=None,
         ylims = None,
         OVERWRITE=False,
         **kwargs):
@@ -41,18 +43,20 @@ def make_a_keo(
     cbar_name: string.
         Label for the colorbar.
     save_or_show: string
-        Defaults to save. You can instead 'show' the plots.
+        Defaults to save. You can instead 'show' or 'return' the plots.
 
     """
     if fname is not None and os.path.exists(fname) and save_or_show == "save":
         if not OVERWRITE:
             raise ValueError("We cannot overwrite the file: " + str(fname))
-    fig = plt.figure(figsize=(10, 7))
+            
+    if save_or_show != 'return':
+        fig, ax = plt.subplots(figsize=(10, 7))
 
-    plt.imshow(
+    data = ax.imshow(
         arr.T,
+        cmap,
         aspect="auto",
-        cmap="viridis",
         origin="lower",
         vmin=cbarlims[0],
         vmax=cbarlims[1],
@@ -60,16 +64,21 @@ def make_a_keo(
         interpolation_stage="rgba",
          **kwargs)
     
-    plt.ylabel(y_label)
-    plt.xlabel(x_label)
-    plt.title(title)
-    plt.colorbar(label=cbar_name)
+    if save_or_show != "return":
+        plt.title(title)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
 
     if ylims is not None:
         plt.ylim(ylims)
 
+    if cbar_name is not None and save_or_show != "return":
+        fig.colorbar(data)
+    elif save_or_show != "return":
+        fig.colorbar(data, label=cbar_lname)
+
     if save_or_show == "return":
-        return(fig)
+        return data
     
     elif save_or_show == "show":
         plt.show()
@@ -106,6 +115,7 @@ def draw_map(
         y_label="Latitude (deg)",
         x_label="Longitude (deg)",
         save_or_show="save",
+        cmap='viridis',
         ax=None,
         fname=None,
         plot_extent=[-180, 180, -90, 90],
@@ -126,11 +136,12 @@ def draw_map(
     world.plot(ax=ax, color="white", edgecolor="black", zorder=1)
     data = ax.imshow(
         data_arr.T,
-        cmap="viridis",
+        cmap,
         aspect="auto",
         extent=plot_extent,
         origin="lower",
         zorder=10,
+        ax=ax,
         alpha=0.8,
         vmin=cbarlims[0],
         vmax=cbarlims[1],
@@ -189,15 +200,6 @@ def draw_map(
                 'save_or_show input is invalid. Accepted inputs are "save",',
                 '"show" or "return", you gave ',
                 save_or_show,)
-
-
-def polar_dial_plot(data,
-                    title = None,
-                    save_or_show = "save",
-                    hemisphere='North',
-                    **kwargs):
-    
-    gs1 = GridSpec(nrows=2, ncols=2, wspace=.1, hspace=.1)
     
 
 
@@ -227,3 +229,123 @@ def interpolate_2d_plot(x,y,c,nx_out, ny_out, map = False):
     p_extent = [min(x), max(x), min(y), max(y)]
 
     return znew, p_extent
+
+
+
+def draw_field_line_plot(x, y, z, title=None, interpolate=False,
+                         x_label='Mlat (Deg)', y_label='Altitude (km)',
+                         x_lims=[-65, 65], y_lims=[0, 1200],
+                         cbar_label=None, cbar_lims=None,
+                         ax = None, cmap = 'viridis',
+                         fname=None, save_or_show='save',
+                         fpeak_col=None):
+    
+    import aacgmv2
+
+    if cbar_lims is None:
+        cbar_lims = [min(z), max(z)]
+        
+    if ax is None and save_or_show != "return":
+        fig, ax = plt.subplots(figsize=(10, 5))
+        
+    elif ax is None and save_or_show == "return":
+        raise ValueError("Cannot return figure if ax is not given.")
+
+    plot_mask = (x >= x_lims[0]) & (x <= x_lims[1]) & \
+        (y >= y_lims[0]) & (y <= y_lims[1])
+
+    x = x[plot_mask]
+    y = y[plot_mask]
+    z = z[plot_mask]
+    if fpeak_col is not None:
+        fpeak_col = fpeak_col[plot_mask]
+
+    if interpolate:
+
+        grid_x, grid_y = np.meshgrid(
+            np.linspace(x_lims[0], x_lims[1], 100),
+            np.linspace(y_lims[0], y_lims[1], 150))
+        in_x, in_y = np.meshgrid(
+            x,y)
+
+        loc_grid = list(zip(x, y))
+        interp = LinearNDInterpolator(
+            loc_grid, z,
+            rescale=True)
+
+        znew = interp(list(zip(grid_x.flatten(), grid_y.flatten())))
+
+        data = ax.imshow(znew.reshape(grid_x.shape), origin='lower',
+                   vmin=cbar_lims[0], vmax=cbar_lims[1], cmap=cmap,
+                   extent=[x_lims[0], x_lims[1], y_lims[0], y_lims[1]],
+                   aspect='auto')
+
+    else:
+        data = ax.scatter(x, y, c=z, vmin=cbar_lims[0], vmax=cbar_lims[1],
+                         cmap=cmap)
+    
+    
+    plt.ylim(y_lims)
+    plt.xlim(x_lims)
+       
+    
+    if save_or_show != 'return':
+        plt.colorbar(label=cbar_label)
+        plt.figtext(.5, .9, title, fontsize=12, ha='center')
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+
+    if fpeak_col is not None:
+        xs = []
+        ys = []
+        bins = np.arange(np.min(x), np.max(x), 3)
+
+        for i in range(len(bins)-1):
+            mlat_mask = (x >= bins[i]) & (x <= bins[i+1])
+            max_here = (np.argmax(fpeak_col[mlat_mask]))
+            xs.append(x[mlat_mask][max_here])
+            ys.append(y[mlat_mask][max_here])
+
+        cubic_interpolation_model = interp1d(xs, ys, kind="quadratic")
+        newx = np.linspace(min(xs), max(xs), 100)
+        newy = cubic_interpolation_model(newx)
+
+        ax.plot(newx, newy, c='k')
+
+    if save_or_show == 'return':
+        return data
+    
+    elif save_or_show == 'show':
+        plt.show()
+        plt.close()
+    elif save_or_show == 'save':
+        if fname is None:
+            raise ValueError('plot save path must be given!')
+        else:
+            # fname = fname.replace(' ','')
+            try:
+                plt.savefig(fname)
+            except FileNotFoundError:
+                try:
+                    directory_list = os.path.join(fname).split('/')[:-1]
+                    os.makedirs(os.path.join(*directory_list))
+                    plt.savefig(fname)
+                except FileExistsError:
+                    # sometimes when we make too many plots in the same
+                    # directory, it fails. this fixes that.
+                    time.sleep(2)
+                    try:
+                        plt.savefig(fname)
+                    except FileNotFoundError:
+                        time.sleep(2)
+                        plt.savefig(fname)
+
+            except FileNotFoundError:
+                print(fname)
+                raise ValueError('File not found error. Check your path.')
+            plt.close()
+
+    else:
+        raise ValueError(
+            'save_or_show input is invalid. Accepted inputs are "save" or',
+            '"show", you gave ', save_or_show)
