aa3302448f7c1bd409cd89c2a5a7feb7caf2c235
Aaron Bukowski <abukowski21@gmail.com>
2023-05-10 15:11:59 -0600

    New utilities.

--- utility_programs/utils.py
+++ utility_programs/utils.py
@@ -58,3 +58,73 @@ def make_ccmc_name(
 
     else:
         return '{}_{}.nc'.format(modelname, ut_str)
+
+
+def get_var_names(dir, models):
+    """Print out a list of variable names.
+
+    Args:
+        dir (str: path-like): directory of outputs
+        models (str/list): name of model.
+    """
+
+    import xarray as xr
+    import os
+    from glob import glob
+
+    if type(models) is str:
+        models = [models]
+
+    for model in models:
+        print(model)
+        files = glob(os.path.join(dir, model + '*.nc'))
+        ds = xr.open_dataset(files[0])
+        print(ds.data_vars)
+        print('\n\n')
+        ds.close()
+
+
+def autoread(file_list,
+             columns_to_return=None,
+             concat_dim='time',
+             ):
+    """Automatically read in a list of files and concatenate them.
+
+    Args:
+        file_list (str or list of paths): List of files to read in.
+        columns_to_return (str or list, optional):
+            Columns (data_vars) to return. Defaults to None.
+        concat_dim (str, optional): concat along this dimension.
+                No reason to ever change. Defaults to 'time'.
+
+    Raises:
+        ValueError: Column not found in files.
+
+    Returns:
+        xarray.Dataset: Dataset holding requested variable(s).
+    """
+
+    import xarray as xr
+
+    if type(file_list) is str:
+        file_list = [file_list]
+
+    ds0 = xr.open_dataset(file_list[0])
+    drops = []
+    for v in ds0.data_vars:
+        if v not in columns_to_return:
+            drops.append(v)
+
+    if len(drops) == len(ds0.data_vars):
+        raise ValueError('No variables to return!\n '
+                         'You gave: {}\n'
+                         'and available variables are: {}'
+                         .format(columns_to_return, ds0.data_vars))
+
+    ds = []
+    for filename in file_list:
+        ds.append(xr.open_dataset(filename,
+                                  drop_variables=drops))
+
+    ds = xr.concat(ds, dim=concat_dim)
+    return ds
