1a5af7e98e1e5fe47cd5979fddfd5ccb45573952
Aaron Bukowski <abukowski21@gmail.com>
2023-05-17 11:19:19 -0600

    Default to not all columns in SAMI outputs.
    Will move numba conditions in a sec to clean it up...

--- PostProcessModelResults.py
+++ PostProcessModelResults.py
@@ -201,7 +201,9 @@ def main(args):
                                 use_ccmc=args.ccmc,
                                 split_by_time=args.ccmc,
                                 split_by_var=not args.ccmc,
-                                numba=numba_installed and not args.low_mem,)
+                                numba=numba_installed and not args.low_mem,
+                                cols=['edens','o+dens','n+dens','n2+dens','etemp',
+                                    'mer_exb','zon_exb'])
 
     return
 
