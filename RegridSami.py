989d4217a98a317385a46b7f057764cca16d3cb1
Aaron Bukowski <abukowski21@gmail.com>
2023-05-22 13:45:09 -0600

    docstring.

--- RegridSami.py
+++ RegridSami.py
@@ -253,6 +253,65 @@ def main(
         split_by_time=True,
         use_ccmc=False,
         numba=False):
+    """Interpolate SAMI3 outputs to a new grid.
+
+    Args:
+        sami_data_path (str, path-like):
+            Path to SAMI3 output files.
+        out_path (str; path-like, optional):
+            Location to save output files. Defaults to "./"
+        save_weights (bool, optional):
+            Whether or not to save weight and index files. Defaults to True.
+        use_saved_weights (bool, optional):
+            Read in existing weight & index files (if they exist). Will not
+                error if files don't exist, just create them.
+                Defaults to True.
+        apply_weights (bool, optional):
+            Whether or not to apply weights. Otherwise weight files are made
+                and the program exits. Defaults to True.
+        cols (str or list-like, optional):
+            Columns to read data from. Can be string or list-like.
+                Defaults to 'all'.
+        dtime_sim_start (datetime.datetime, optional):
+            Datetime of simulation start. Required to read raw SAMI outputs.
+                Defaults to None.
+        lat_step (int, optional):
+            Integer step to use in output grid (deg). Defaults to 2.
+        lon_step (int, optional):
+            Integer step to use in output grid (deg). Defaults to 5.
+        alt_step (int, optional):
+            Integer step to use in output grid (in km). Defaults to 50.
+        minmax_alt (list, optional):
+            Min & Max altitude to output in km. Defaults to [100, 2200].
+        lat_finerinterps (int, optional):
+            Finer interpolation factor to use in latitude.
+            Will by coarsened to the desired output resolution before writing.
+            Defaults to 3.
+        lon_finerinterps (int, optional):
+            Finer interpolation factor to use in longitude.
+            Defaults to 3.
+        alt_finerinterps (int, optional):
+            Finer interpolation factor to use in altitude. Defaults to 2.
+        out_coord_file (_type_, optional):
+            Output coordinates from a file instead of using the default grid.
+            Cannot be used with finerinterps.
+            Defaults to None (no coordinate file).
+        split_by_var (bool, optional):
+            Write each variable to a separate file. Defaults to False.
+        single_file (bool, optional):
+            Write all variables to a single file. Defaults to False.
+        split_by_time (bool, optional):
+            Write each time to a separate file. Defaults to True.
+        use_ccmc (bool, optional):
+            Whether or not to use CCMC naming conventions for outputs. Must be
+                used with split_by_time. Defaults to False.
+        numba (bool, optional):
+            Use numba to speed up applying weights. Defaults to False.
+
+    Raises:
+        ValueError: If out_coord_file does not have the reuired variables.
+        ValueError: If split_by_var and single_file are both True.
+    """
 
     if out_path is None:
         out_path = sami_data_path
@@ -295,12 +354,19 @@ def main(
 
     else:
         ds_out = xr.open_dataset(out_coord_file)
-        latout = ds_out['lat'].values
-        lonout = ds_out['lon'].values
-        altout = ds_out['alt'].values
+        print('Read coordinate file.')
+        try:
+            latout = ds_out['lat'].values
+            lonout = ds_out['lon'].values
+            altout = ds_out['alt'].values
+        except ValueError:
+            raise ValueError('Coordinate file must have lat, lon, alt vars.')
 
         out_cart = latlonalt_to_cart(
             altout, lonout, np.array(altout) + 6371)
+        lon_finerinterps = 1
+        lat_finerinterps = 1
+        alt_finerinterps = 1
 
     if use_saved_weights:
         weights = np.fromfile(os.path.join(out_path, 'weights'))
