67c6bb14d4574d0fc9651736645aa0f4e6463c2e
Aaron Bukowski <abukowski21@gmail.com>
2023-07-05 16:51:46 -0500

    think it's good not. Only works for SAMI, can pass latlonalt in if it's formatted correctly. Needs docstrings

--- interpolate_outputs.py
+++ interpolate_outputs.py
@@ -24,6 +24,7 @@ from utility_programs.utils import str_to_ut, make_ccmc_name
 import argparse
 import pickle
 from scipy.spatial import Delaunay
+from scipy.interpolate import LinearNDInterpolator
 
 
 
@@ -49,20 +50,24 @@ def latlonalt_to_cart(lat, lon, radius):
     
 def do_interpolations(
     sami_data_path=None,
-    gitm_data_path=None,
+    dtime_sim_start=None,
     out_latlonalt=None,
     out_path=None,
     out_runname='',
     save_delauney=False,
-    data_vars=None,
     max_alt=None,
     cols='all',
     show_progress=False,
+    engine='h5netcdf',
     ):
     
 
     # deal with sami first
     if sami_data_path is not None:
+        
+        if type(dtime_sim_start) == str:
+            dtime_sim_start = str_to_ut(dtime_sim_start)
+        
         nz, nf, nlt, nt=SAMI.get_grid_elems_from_parammod(sami_data_path)
         old_shape=[nlt, nf, nz]
         grid=SAMI.get_sami_grid(sami_data_path, nlt, nf, nz)
@@ -104,11 +109,18 @@ def do_interpolations(
 
             out_latlonalt=latlonalt_to_cart(
                 out_lats, out_lons, np.array(out_alts) + 6371)
+        else:
+            out_latlonalt=latlonalt_to_cart(
+                out_latlonalt[0], out_latlonalt[1],
+                np.array(out_latlonalt[2]) + 6371)
         
-        if os.path.exists(os.path.join(sami_data_path, 'delauney.pkl')):
+        if os.path.exists(os.path.join(sami_data_path, 
+                                       'delauney_max-%i.pkl' %max_alt)):
             if save_delauney:
                 print('attempting to reuse existing triangulation file')
-                tri = pickle.load(os.path.join(sami_data_path, 'delauney.pkl'))
+                with open(os.path.join(
+                    sami_data_path, 'delauney_max-%i.pkl' %max_alt), 'rb') as f:
+                    tri = pickle.load(f)
             else:
                 print('Found existing triangulation file. Recalculating...',
                      '\n(Specify save_delauney=True to reuse)')
@@ -118,29 +130,43 @@ def do_interpolations(
             tri = Delaunay(in_cart)
             if save_delauney:
                 print('Saving')
-                pickle.dumps(tri, os.path.join(sami_data_path, 'delauney.pkl'))
+                with open(os.path.join(sami_data_path,
+                                       'delauney_max-%i.pkl' %max_alt),
+                          'wb') as f:
+                    pickle.dump(tri, f)
         
+        #format 'cols' variable
         if cols == 'all':
-            cols = SAMI.sami_og_vars
+            cols = SAMI.sami_og_vars.items()
         else:
-            cols = np.asarray(cols)
-            
+            if type(cols) == str:
+                cols = [cols]
+            else:
+                cols = np.asarray(cols)
+                
         if show_progress:
-            pbar = tqdm.progress(total=len(cols)*nt)
+            pbar = tqdm(total=len(cols)*nt,
+                       desc='Reading in SAMI data')
             
         first=True #for choosing which mode to write
         for data_var in cols:
+            interpd = []
             data, times=SAMI.read_to_nparray(
-                sami_data_path, datetime(2011,3,16),
-                cols='edens',
+                sami_data_path, dtime_sim_start,
+                cols=data_var,
                 skip_time_check=True)
+            
+            if show_progress:
+                pbar.set_description('interpolating')
             for t in tqdm(range(len(times))):
                 interp = LinearNDInterpolator(
                     tri,
-                    data['data']['edens'][:,:,:,t][mask].flatten())
+                    data['data'][data_var][:,:,:,t][mask].flatten())
                 interpd.append(interp(out_latlonalt.T))
                 if show_progress:
                     pbar.update()
+            if show_progress:
+                pbar.set_description('writing Dataset...')
             ds=xr.Dataset(coords={
                 'time': (['time'], times),
                 'alt': (['alt'], altout),
@@ -154,10 +180,11 @@ def do_interpolations(
                              len(altout)))
             ds.to_netcdf(os.path.join(
                 out_path,'SAMI_REGRID'+out_runname),
-                         engine='h5netcdf',
+                         engine=engine,
                          mode='w' if first else 'a',
                          encoding={'time':{'dtype':float}})
             first=False
+            del ds, interpd, data #clean up memory
 
     
 # if __name__ == '__main__':
