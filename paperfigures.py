b7cb4f6aae2f09b6d39db59f668f09691969eae7
Aaron Bukowski <abukowski21@gmail.com>
2023-08-07 15:11:26 -0600

    Figures updated
    Formatting, correct axis labels and geo 0-360 -> -180/180 transformations, better labels, etc.

--- paperfigures.py
+++ paperfigures.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from utility_programs.utils import ut_to_lt
+from matplotlib.dates import DateFormatter
+
 
 
 def timedeltatotime(td, secs_back=False):
@@ -39,9 +41,10 @@ def fig1(ds,
          plot_start,
          plot_delta,
          storm_start,
-         at_alt,
+         at_alt=None,
          numplots=6,
          suptitle_colname='Total Neutral Mass Density',
+         mag_eq=None,
          ):
 
     fig, axs = plt.subplots(3, 2, figsize=(15, 10), subplot_kw={
@@ -74,6 +77,13 @@ def fig1(ds,
         lines.left_labels = True if t == 0 or t == 2 or t == 4 else False
 
         axes[t].add_feature(Nightshade(tnow, alpha=0.21))
+        
+        if mag_eq is not None:
+            axes[t].plot(mag_eq['glon'], mag_eq['mlat'], transform=ccrs.PlateCarree(),
+                         linestyle='--', color='k')
+        
+        
+        
         tnow += plot_delta
         t += 1
 
@@ -95,10 +105,11 @@ def fig1(ds,
 def fig2(ds,
          vlims,
          lons,
-         at_alt,
+         at_alt=None,
          tlims=None,
          numplots=6,
          gitm=True,
+         mag_eq=None,
          ):
 
     fig, axs = plt.subplots(3, 2, figsize=(15, 10),
@@ -115,6 +126,10 @@ def fig2(ds,
         ds = ds.sel(time=slice(tlims[0], tlims[1]))
 
     while t < numplots:
+        added_lon = False
+        if ds.sel(lon=lons[t], method='nearest').sum() == 0:
+            lons[t] += 4
+            added_lon = True
         im = ds.sel(lon=lons[t], method='nearest').plot(x='time',
                                                         vmin=-vlims,
                                                         vmax=vlims,
@@ -122,30 +137,64 @@ def fig2(ds,
                                                         add_colorbar=False,
                                                         cmap='bwr'
                                                         )
+        if added_lon:
+            lons[t] -=4
 
-        axes[t].set_title("Geographic Longitude = %i Degrees" %
+        if lons[t] < 180 and lons[t] > 0:
+            axes[t].set_title("Geographic Longitude = %i° East" %
                           int(lons[t]), fontsize='x-large')
+        elif lons[t] > 180:
+            axes[t].set_title("Geographic Longitude = %i° West" %
+                          (360-int(lons[t])), fontsize='x-large')
+        else:
+            axes[t].set_title("Geographic Longitude = %i°" %
+                          int(lons[t]), fontsize='x-large')
+            
+        axes[t].xaxis.set_major_formatter(DateFormatter('%H:%M'))
+        
+        axes[t].vlines(pd.Timestamp('2011-05-21 12:00:00'),-90,90,
+                       color='peru', linestyle = 'solid', linewidth=3)
+        
+        lts = ut_to_lt(ds.time.values, lons[t])
+        dawn = np.argmin(np.abs(lts-6))
+        dusk = np.argmin(np.abs(lts-18))
+        if dawn > 2 and dawn < len(ds.time.values)-2:
+            axes[t].vlines(ds.time.values[dawn],-90,90,
+                      color='lime', linestyle = 'dotted', linewidth=3)
+        if dusk > 2 and dusk < len(ds.time.values)-2:
+            axes[t].vlines(ds.time.values[dusk],-90,90,
+                      color='lime', linestyle = 'dashed', linewidth=3)
+            
+        if mag_eq is not None:
+            axes[t].hlines(
+                mag_eq['mlat'][np.argmin(np.abs(mag_eq['glon'] - lons[t]))],
+                ds.time.values[0], ds.time.values[-1],
+                            linestyle='--', color='k')
+            
+        # axes[t].axvspan(ds.time.values[dawn], ds.time.values[dawn], alpha=0.15, color='k')
 
         axes[t].set_ylabel('')
         axes[t].set_xlabel('')
         t += 1
 
-    fig.supxlabel('Time from Storm Onset (HH:MM)', fontsize='x-large')
+    fig.supxlabel('Universal Time (HH:MM)', fontsize='x-large')
     fig.supylabel('Latitude (Degrees North)', fontsize='x-large')
-    
+
     if gitm:
         fig.suptitle("Perturbation Total Neutral Mass Density from GITM at %ikm" % at_alt,
                  fontsize='xx-large', fontweight='heavy')
     else:
-        fig.suptitle("Perturbation TEC from SAMI at %ikm" % at_alt,
+        fig.suptitle("Perturbation TEC from SAMI",
                  fontsize='xx-large', fontweight='heavy')
 
     fig.tight_layout()
 
     cbar = fig.colorbar(im, ax=axs.ravel().tolist(), orientation="vertical",
                         aspect=40, extend='both',)
-    cbar.set_label('% Over Background', fontsize='x-large')
-
+    if gitm:
+        cbar.set_label('% Over Background', fontsize='x-large')
+    else:
+        cbar.set_label('Perturbation TEC (TECUnits)', fontsize='x-large')
     return fig
 
 
@@ -163,9 +212,9 @@ def fig3(da,
     lons = np.arange(0, 360)
     lts = ut_to_lt([time_here], lons)
     central_lon = lons[np.argmin(np.abs(24-lts))]
-    
-    fig = plt.figure(figsize=(10,4))
-    ax1 = fig.add_subplot(1,2,1,
+
+    fig = plt.figure(figsize=(10, 4))
+    ax1 = fig.add_subplot(1, 2, 1,
                           projection=ccrs.Orthographic(central_lon, 90))
     da.plot(ax=ax1, x='lon', transform=ccrs.PlateCarree(),
                    cmap=dial_cmap,
@@ -175,8 +224,10 @@ def fig3(da,
     ax1.add_feature(Nightshade(time_here), alpha=0.3)
     ax1.gridlines(color='black', linestyle='--',
                               alpha=0.6, draw_labels=True)
+    ax1.coastlines(zorder=3, color='black', alpha=1)
 
-    ax2 = fig.add_subplot(1,2,2,
+
+    ax2 = fig.add_subplot(1, 2, 2,
                           projection=ccrs.Orthographic(
                               central_lon-180, -90))
     im = da.plot(ax=ax2, x='lon', transform=ccrs.PlateCarree(),
@@ -186,14 +237,15 @@ def fig3(da,
     ax2.add_feature(Nightshade(time_here), alpha=0.3)
     ax2.gridlines(color='black', linestyle='--',
                               alpha=0.6, draw_labels=True)
-    
+    ax2.coastlines(zorder=3, color='black', alpha=1)
     fig.suptitle(suptitle, fontsize='xx-large', fontweight='heavy')
 
     fig.tight_layout()
-    
+
     cbar = fig.colorbar(im, ax=[ax1, ax2], orientation="vertical",
-                        aspect=40,)
-    cbar.set_label('Altitude Integrated \nJoule Heating ($\mathrm{W/M^2}$)', fontsize='x-large')
+                        aspect=17, extend='max')
+    cbar.set_label(
+        'Altitude Integrated \nJoule Heating ($\mathrm{W/M^2}$)', fontsize='x-large')
 
     return fig
 
@@ -201,16 +253,72 @@ def fig3(da,
 def fig4(ds,
          vlims,
          lons,
-         at_alt,
          tlims=None,
          numplots=6,
          gitm=False,
+         mag_eq=None
          ):
+    
     fig = fig2(ds,
          vlims,
          lons,
-         at_alt,
          tlims=tlims,
          numplots=numplots,
-         gitm=False,)
+         gitm=False,
+              mag_eq=mag_eq)
+    return fig
+
+
+def fig5(ds,
+         vlims,
+         times,
+         storm_start,
+         lon,
+         numplots=6,
+         ):
+
+    fig, axs = plt.subplots(3, 2, figsize=(13, 10),
+                            sharex=True,
+                            sharey=True,)
+
+    # plotting...
+    # Setup some things...
+    t = 0
+
+    axes = axs.flatten()
+
+    while t < numplots:
+        im = ds.sel(time=times[t], method='nearest').interp(
+            lat=np.arange(-90, 90, 1),
+            alt=np.arange(150, 1500, 20)).plot(x='lat',
+                                                        vmin=-vlims,
+                                                        vmax=vlims,
+                                                        ax=axes[t],
+                                                        add_colorbar=False,
+                                                        cmap='bwr'
+                                                        )
+
+        axes[t].set_title("%s From Storm Onset (%s Local Time)"
+                          %(timedeltatotime(times[t]-storm_start),
+                            ut_to_lt([times[t]], lon)[0]), fontsize = 'x-large')
+
+        axes[t].set_ylabel('')
+        axes[t].set_xlabel('')
+        axes[t].set_ylim(100, 1000)
+        t += 1
+
+    fig.supxlabel('Latitude (Degrees North)', fontsize = 'x-large')
+    fig.supylabel('Altitude (km)', fontsize = 'x-large')
+
+    fig.suptitle("Perturbation Electron Density from SAMI\n"
+                 "Along %s Geographic Longitude"
+                 %(str(int(lon)) + "° East" if lon < 180 else str(360-int(lon)) + "° West"),
+                 fontsize = 'xx-large', fontweight = 'heavy')
+
+    fig.tight_layout()
+
+    cbar=fig.colorbar(im, ax = axs.ravel().tolist(), orientation = "vertical",
+                        aspect = 40, extend = 'both',)
+    cbar.set_label('% Over Background', fontsize = 'x-large')
+
     return fig
