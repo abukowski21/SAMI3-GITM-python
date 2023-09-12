9fd0ff73cd6c3f0c83b4dd6f44a94fb460b35fcb
Aaron Bukowski <abukowski21@gmail.com>
2023-09-12 08:13:11 -0600

    update deps of docs
    nbsphinx is known to have issues. switch to myst_nb

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.autosummary',
-    'nbsphinx']
+    'myst_nb']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
