41ac80d17e38a4725d491c350d96b2e0b91b18e4
Aaron Bukowski <abukowski21@gmail.com>
2023-11-08 15:27:06 -0600

    add a todo item in utility.read.SAMI.read_to_xarray

--- utility_programs/read_routines/SAMI.py
+++ utility_programs/read_routines/SAMI.py
@@ -541,6 +541,8 @@ def read_raw_to_xarray(sami_data_path, dtime_sim_start, cols='all',
                        start_dtime=None, end_dtime=None,
                        start_idx=None, end_idx=None,
                        progress_bar=False, skip_time_check=False):
+    print('TODO: THE TIME BAR IS WRONG')
+
     """
     Read in (raw) SAMI data and return an xarray dataset.
         ! This only works on raw, pre-processed SAMI data !
